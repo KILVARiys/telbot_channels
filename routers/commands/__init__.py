d2d2b56001ba8149ad73062ed88798870c12f4ad
Admin <seremenko135@gmail.com>
2024-10-05 19:23:05 +0300

    start

--- routers/commands/__init__.py
+++ routers/commands/__init__.py
@@ -3,9 +3,11 @@ __all__ = ("router",)
 from aiogram import Router
 
 from .base_command import router as base_commands_router
+from .func_pay import router as pay_router
 
 router = Router(name=__name__)
 
 router.include_routers(
     base_commands_router,
+    pay_router,
 )
